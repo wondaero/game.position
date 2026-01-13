fb663b55bdc9958423259c1fcb42b0f8e819afeb
wondaero <wondaero@nate.com>
2026-01-14 07:33:39 +0900

    1차 커밋밋

---
+++ script.js
@@ -0,0 +1,23 @@
+const state = {
+    rows: '12345',
+    cols: 'abcde',
+    stage: 1,
+    cells: 25
+}
+
+function getRandomInt(mn, mx) {
+    return Math.floor(Math.random() * (mx - mn + 1)) + mn;
+}
+
+function setGame(stage){
+    stage = state.stage;
+}
+
+function getRandomCell(cnt){
+    let cells = [];
+    for(let i = 0; i < cnt; i++){
+        
+    }
+
+
+}
